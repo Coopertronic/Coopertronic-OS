782b9c1364c638d9ef16dbc29e8a8250f60cec6b
Matthew Cooper <coopertronics@gmail.com>
2023-07-10 20:52:24 +0100

    Added a few more packages and enabled the services so that it boots with sddm live and network manager.

---
+++ releng/airootfs/root/customize_airootfs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e -u
+
+sed -i 's/#\(en_GB\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+usermod -s /usr/bin/zsh root
+cp -aT /etc/skel /root/
+chmod 700 /root
+#   unset the root password
+passwd -d root
+
+sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/sshd_config
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+loadkeys uk
+systemctl enable sddm
+systemctl set-default graphical.target
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable org.cups.cupsd
