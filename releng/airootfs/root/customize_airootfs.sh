c32a2ae03bab4cb88d275d59d3841d3333fa6e03
Matthew Cooper <coopertronics@gmail.com>
2023-08-03 13:17:12 +0100

    Try again

--- releng/airootfs/root/customize_airootfs.sh
+++ releng/airootfs/root/customize_airootfs.sh
@@ -13,7 +13,8 @@ passwd -d root
 
 #sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
-localectl set-keymap --no-convert uk
+#localectl set-keymap --no-convert uk
+#locale-gen
 systemctl enable sddm
 systemctl set-default graphical.target
 systemctl enable NetworkManager
