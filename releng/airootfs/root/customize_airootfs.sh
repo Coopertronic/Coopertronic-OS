7320aad1193d4d180d6d4b7b66efcb969fc66dbb
Matthew Cooper <coopertronics@gmail.com>
2024-04-15 14:52:38 +0100

    New version 99. trying to get trinituy desktop in there.

--- releng/airootfs/root/customize_airootfs.sh
+++ releng/airootfs/root/customize_airootfs.sh
@@ -16,9 +16,11 @@ sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 #localectl set-keymap --no-convert uk
 #locale-gen
 pacman-key --init
-pacman-key --populate archlinux
 pacman-key --recv-keys 515A202B640F3678
 pacman-key --lsign 515A202B640F3678
+pacman-key --recv-key  D6D6FAA25E9A3E4ECD9FBDBEC93AF1698685AD8B
+pacman-key --lsign-key D6D6FAA25E9A3E4ECD9FBDBEC93AF1698685AD8B
+pacman-key --populate archlinux
 pacman-key --populate ctos
 pacman -Syy
 #pacman -S ctos-calamares-settings --overwrite '*' --noconfirm
